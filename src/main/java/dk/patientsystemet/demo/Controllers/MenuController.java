9eca5629bcabc9135db04399e7257d1726bfae7a
Nick Hansen <nick4630@stud.kea.dk>
2019-04-01 10:00:47 +0200

    login css

--- src/main/java/dk/patientsystemet/demo/Controllers/MenuController.java
+++ src/main/java/dk/patientsystemet/demo/Controllers/MenuController.java
@@ -1,4 +1,13 @@
 package dk.patientsystemet.demo.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MenuController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
